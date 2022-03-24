12f094c7793aae2f871221c8fa63b130f494f39f
LeonardoCGoulart <joedarkalone@hotmail.com>
2022-03-24 14:07:48 -0300

    teste propriedade bilhete e quantidade dezena passando

---
+++ src/model/Bilhete.js
@@ -0,0 +1,45 @@
+module.exports = class Bilhete{
+
+    constructor(id, quantidadeNumerosApostados, preco){
+        this.id = id;
+        this.quantidadeNumerosApostados = quantidadeNumerosApostados;
+        this.preco = preco
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    setId(id){
+        this.id = id;
+    }
+
+    getQuantidadeNumerosApostados(){
+        if(this.quantidadeNumerosApostados < 6 || this.quantidadeNumerosApostados > 15){
+            throw new Error('jogo invalido')
+        }
+        return this.quantidadeNumerosApostados;
+    }
+
+    setQuantidadeNumerosApostados(quantidadeNumerosApostados){
+        if(quantidadeNumerosApostados < 6 || quantidadeNumerosApostados > 15){
+            throw new Error('jogo invalido')
+        } 
+        this.quantidadeNumerosApostados = quantidadeNumerosApostados;
+    }
+
+    getPreco(){
+        if(this.preco < 0){
+            throw new Error('preco invalido')
+        }
+        return this.preco;
+    }
+
+    setPreco(preco){
+        if(preco < 0){
+            throw new Error('preco invalido')
+        } 
+        this.preco = preco;
+    }
+
+}
--- test/main.test.js
+++ test/main.test.js
@@ -1,3 +1,5 @@
+const Bilhete = require('../src/model/Bilhete')
+
 describe('Quantidade de numeros para a aposta', () => {
 
     test('aposta com 6 numeros', () => {
@@ -20,6 +22,69 @@ describe('Quantidade de numeros para a aposta', () => {
         }).toThrow('jogo invalido');
     });
     
+});
 
+describe('Testando propriedades', () => {
+
+    test('get ID', () => {
+        bilhete = new Bilhete(1, 6, 4.50); // ID/ numero/ preço
+        expect(bilhete.getId()).toBe(1); 
+    });
+
+    test('set ID', () => {
+        bilhete = new Bilhete(1, 6, 4.50); // ID/ numero/ preço
+        bilhete.setId(2)
+        expect(bilhete.getId()).toBe(2); 
+    });
+    test('get preco', () => {
+        bilhete = new Bilhete(1, 6, 4.50); // ID/ numero/ preço
+        expect(bilhete.getPreco()).toBe(4.50); 
+    });
+
+    test('set preco', () => {
+        bilhete = new Bilhete(1, 6, 4.50); // ID/ numero/ preço
+        bilhete.setPreco(2)
+        expect(bilhete.getPreco()).toBe(2); 
+    });
+
+    test('preco < 0', () => {
+        bilhete = new Bilhete(1, 6, 4.50); // ID/ numero/ preço
+        expect(() => {
+            bilhete.setPreco(-1)
+        }).toThrow('preco invalido');
+    });
+    
+    test('preco < 0', () => {
+        bilhete = new Bilhete(1, 6, -1); // ID/ numero/ preço
+        expect(() => {
+            bilhete.getPreco()
+        }).toThrow('preco invalido');
+    });
+
+    test('get numeros Apostados', () => {
+        bilhete = new Bilhete(1, 6, 4.50); // ID/ numero/ preço
+        bilhete.setPreco(2)
+        expect(bilhete.getQuantidadeNumerosApostados()).toBe(6); 
+    });
+
+    test('set numeros Apostados invalido', () => {
+        bilhete = new Bilhete(1, 6, 4.50); // ID/ numero/ preço
+        expect(() => {
+            bilhete.setQuantidadeNumerosApostados(5)
+        }).toThrow('jogo invalido');
+    });
+
+    test('set numeros Apostados invalido', () => {
+        bilhete = new Bilhete(1, 6, 4.50); // ID/ numero/ preço
+        expect(() => {
+            bilhete.setQuantidadeNumerosApostados(16)
+        }).toThrow('jogo invalido');
+    });
+
+    test('set numeros Apostados valido', () => {
+        bilhete = new Bilhete(1, 6, 4.50); // ID/ numero/ preço
+        bilhete.setQuantidadeNumerosApostados(7)
+        expect(bilhete.getQuantidadeNumerosApostados()).toBe(7); 
+    });
 
 });
